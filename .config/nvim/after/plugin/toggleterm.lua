5963b125ff2eb45e95526fcb0ae13b59cc68819b
Thomaz Andrade <thomaz@tnex.xyz>
2023-09-17 13:41:10 -0400

    updated terminal toggle to ctrl+t

--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -1,7 +1,7 @@
 -- vim.keymap.set("n", "<leader>t", ":ToggleTerm direction=float<cr>")
 
 require("toggleterm").setup {
-  open_mapping = [[<leader>t]],
+  open_mapping = [[<c-t>]],
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   shade_terminals = true,
@@ -29,3 +29,15 @@ require("toggleterm").setup {
     }
   }
 }
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
